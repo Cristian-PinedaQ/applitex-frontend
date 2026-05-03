a1cd9875849319cca77ba261094dfa006840ace3
Cristian-PinedaQ <crispdevps@gmail.com>
2026-05-03 00:15:44 -0500

    lint: config to warnings only

--- eslint.config.js
+++ eslint.config.js
@@ -22,5 +22,7 @@ export default tseslint.config({
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
   },
 })
